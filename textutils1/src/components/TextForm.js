d2012e42c8b21ab2fc764857f2ce828978c1e6d6
Jeet <shindejeet1505@gmail.com>
2023-06-19 10:55:38 +0530

    Initial commit

---
+++ textutils1/src/components/TextForm.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function TextForm() {
+    return (
+        <div>
+            <div className="mb-3">
+                <label for="example for controlTextarea1" class="form-label">Enter text below.</label>
+                <textarea className="form-control" id="exampleFormcontrolTextarea1" cols="30" rows="5"></textarea>
+
+            </div>
+        </div>
+    )
+}
